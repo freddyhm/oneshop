e326b3cd1262177fd0151cca26834fb84697777a
Freddy <freddy@.hm@growple.com>
2013-07-28 18:38:11 -0400

    added hover on catalog products

--- skin/frontend/default/oneshop/js/oneshop.js
+++ skin/frontend/default/oneshop/js/oneshop.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function(){
+	
+	// init slider on homepage
 	jQuery('.flexslider').flexslider({
-    slideshow: "false"
-  });
+   		 slideshow: "false"
+ 	});
+
+	// show/hide product name, price, overlay
+	jQuery(".item").hover(
+		function(){
+			jQuery(".product-name", this).show();
+			jQuery(".catalog-overlay", this).show();
+			jQuery(".price", this).show();
+		}, function(){
+			jQuery(".product-name", this).hide();
+			jQuery(".catalog-overlay", this).hide();
+			jQuery(".price", this).hide();
+		}
+	);
 });

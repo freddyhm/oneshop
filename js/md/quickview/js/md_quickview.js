5017f1804375ff362cd31c33dbd4264fca4eada3
dh9kim <kevin.kim@growple.com>
2013-08-21 23:15:18 -0400

    Changes in Shop overlay css

--- js/md/quickview/js/md_quickview.js
+++ js/md/quickview/js/md_quickview.js
@@ -108,8 +108,8 @@ jQuery(function($) {
 		//insert quickview popup
 		$('#md_quickview_handler').fancybox({
 				'titleShow'			: false,
-				'width'				: EM.Quickview.QS_FRM_WIDTH,
-				'height'			: 'auto',//EM.Quickview.QS_FRM_HEIGHT,
+				'width'				: 700,
+				'height'			: 350,//EM.Quickview.QS_FRM_HEIGHT,
 				'autoScale'			: false,
 				'transitionIn'		: 'none',
 				'transitionOut'		: 'none',
